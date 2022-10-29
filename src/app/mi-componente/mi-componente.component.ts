81461759f501b53a08ae679b256918a614ceabe7
alejandrotoro18 <43111688+alejandrotoro18@users.noreply.github.com>
2022-10-29 11:47:53 -0300

    3 files modified

--- src/app/mi-componente/mi-componente.component.ts
+++ src/app/mi-componente/mi-componente.component.ts
@@ -10,13 +10,19 @@ export class MiComponenteComponent implements OnInit {
   public titulo:String;
   public comentario:String;
   public year: number;
+  public mostrarPeliculas:boolean;
 
   constructor() { 
     this.titulo="Hola Mundo soy mi componente"
     this.comentario="salida soy mi comentario"
     this.year=2022
+    this.mostrarPeliculas=true;
 
     console.log("Componente mi-componente cargado");
+    console.log(this.titulo,this.comentario,this.year);
+  }
+  ocultarPeliculas(){
+    this.mostrarPeliculas=false;
   }
 
   ngOnInit() {
